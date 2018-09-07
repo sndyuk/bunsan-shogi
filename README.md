88aecf3426e278b599b02ef872af6329d30ca5ee
sndyuk <sanada@sndyuk.com>
2018-09-07 18:59:03 +0900

    Update README

--- README.md
+++ README.md
@@ -1,10 +1,19 @@
 # Scala Shogi Engine
+Shogi library for Scala.
 
-### Run a sample application.
+### Try sample applications
 
 ```sh
 $ sbt "project sample" run
 ```
+
+### Performance
+Scala 2.12 / 2.9 GHz Intel Core i7
+
+* 700K NPS / 1 Thread
+* 1,700K NPS / 2 Threads
+* 2,400K NPS / 4 Threads
+
 
 ### License
 [Apache License, Version 2.0](LICENSE)
--- modules/sample/src/main/scala/jp/sndyuk/shogi/ai/UCB.scala
+++ modules/sample/src/main/scala/jp/sndyuk/shogi/ai/UCB.scala
@@ -10,7 +10,7 @@ class UCB extends AI {
 
   private val ucbConst = 0.5d
 
-  private val conf = Config(maxDepth = 125, maxQueueSize = 20000, thread = Math.max(1, Runtime.getRuntime.availableProcessors() / 2), (scores: List[Plan]) => {
+  private val conf = Config(maxDepth = 125, maxQueueSize = 20000, thread = 4, (scores: List[Plan]) => {
     scores.map { result =>
       val rate = result.win / Math.max(result.playouts, 1)
       val ucb = rate + ucbConst * Math.sqrt((2 * Math.log(result.playouts)) / result.playouts)
